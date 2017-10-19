8f57747e8cebee45afb445e68a5f8349fac845a0
Jacqlene Sifre <jsifre01@gmail.com>
2017-10-19 20:43:07 +0000

    Done.

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,46 @@
 class LandmarksController < ApplicationController
 
+  get '/' do
+    redirect "/landmarks"
+  end
+
+  get '/landmarks' do
+    @landmarks = Landmark.all
+
+    erb :'/landmarks/index'
+  end
+
+  get '/landmarks/new' do
+
+    erb :'/landmarks/new'
+  end
+
+  get '/landmarks/:id' do
+    @landmark = Landmark.find(params[:id])
+
+    erb :'/landmarks/show'
+  end
+
+  get '/landmarks/:id/edit' do
+    @landmark = Landmark.find(params[:id])
+
+    erb :'/landmarks/edit'
+  end
+
+  post '/landmarks' do
+
+    @landmark = Landmark.create(params[:landmark])
+    @landmark.save
+
+    redirect("/landmarks/#{@landmark.id}")
+  end
+
+  post '/landmarks/:id' do
+    @landmark = Landmark.find_by(params[:id])
+    @landmark.update(params[:landmark])
+    @landmark.save
+
+    redirect("/landmarks/#{@landmark.id}")
+  end
+
 end
